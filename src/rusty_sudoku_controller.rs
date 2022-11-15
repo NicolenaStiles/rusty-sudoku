dc97239d422b934ecd24ddc38fedcdcc21cc53a1
NicolenaStiles <nicolena.stiles@gmail.com>
2022-11-15 17:46:02 -0500

    Moving files around and cleaning up.

--- src/rusty_sudoku_controller.rs
+++
@@ -1,3 +0,0 @@
-// Controller for the MVC rust sudoku solver framework
-// (might end up just being handled by TUI anyway, but it'd be cool
-// if I could wrap some of that functionality in here for the sake of it)
--- src/rusty_sudoku_view.rs
+++
@@ -1,8 +0,0 @@
-// View printer for Rust-based sudoku solver
-// (Assumes you're using a terminal because I'm still pretty basic)
-
-mod rusty_sudoku_model;
-
-pub fn print_sudoku_puzzle(input : Puzzle ) {
-    println!("{:?}", input.initial_state);
-}
