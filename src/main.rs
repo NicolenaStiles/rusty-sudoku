decffa90bb580e2ee362d8d18a15d184030908c5
NicolenaStiles <nicolena.stiles@gmail.com>
2022-11-15 13:09:14 -0500

    Added support for difficulty notation.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 // Main Application
 ///////////////////////////////////////////////////////////////////////////////
 
+// General/overall imports:
+// imports for VoronoiPoint
+use std::fmt; // for debug
 use std::error::Error;
 use std::fs::File;
 
@@ -24,10 +27,32 @@ fn load_puzzles(filepath: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 */
+// field UnitType for VoronoiPoint
+#[derive(Copy,Clone)]
+enum Difficulty {
+    Undefined,
+    Easy,
+    Medium,
+    Hard
+}
+
+// debug implementation for UnitType enum
+impl fmt::Debug for Difficulty {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Difficulty::Undefined => write!(f, "Undefined"),
+            Difficulty::Easy => write!(f, "Easy"),
+            Difficulty::Medium => write!(f, "Medium"),
+            Difficulty::Hard => write!(f, "Hard"),        
+        }
+    }
+}
 
 struct Puzzle {
     unsolved: Vec<u8>,
-    solved: Vec<u8>
+    solved: Vec<u8>,
+    current_state: Vec<u8>, 
+    difficulty: Difficulty
 }
 
 // Converts single string into vector of u8 ints
@@ -38,6 +63,7 @@ fn string_to_int_vec() {
 fn main() {
     // Create a CSV parser that reads data from stdin.
     let rdr = csv::Reader::from_path("./src/puzzles.csv");
+
     // Loop over each record.
     for result in rdr.unwrap().records() {
         // An error may occur, so abort the program in an unfriendly way.
@@ -52,12 +78,11 @@ fn main() {
 
         // converts from single string into array of bytes, then from bytes to base values
         // (48 is constant to convert 0-9 numbers from ascii value)
-        let byte_vector = record[0].as_bytes();
+        let byte_vector = record[1].as_bytes();
         let mut final_vector: Vec<u8> = vec![0; 81];
         for (i, val) in byte_vector.iter().enumerate() {
             final_vector[i] = *val - 48;
         }
-        println!("byte vector: {:?}", byte_vector);
         println!("final vector: {:?}", final_vector);
 
     }
